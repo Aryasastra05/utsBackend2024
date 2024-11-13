9380b631016d735fda25a0bd6b12993d38d5bb79
Aryasastra05 <aryasastra05go@gmail.com>
2024-11-13 15:55:58 +0700

    pushlagi

--- app/Http/Controllers/EmployeesController.php
+++ app/Http/Controllers/EmployeesController.php
@@ -38,13 +38,14 @@ class EmployeesController extends Controller
       
       } else {
           $input =[
-              'nama pegawai' => $request->nama,
+              'nama pegawai' => $request->name,
               'jenis kelamin pegawai'=> $request->gender,
               'no hp pegawai'=> $request->phone,
-              'alamat pegawai'=> $request->addres,
+              'alamat pegawai'=> $request->address,
               'email pegawai' => $request->email,
               'status pegawai' => $request->status,
               'tanggal masuk kerja' => $request->hired_on,
+              
           ];
           $employees = Employees::create($input);
           $data = [
@@ -56,4 +57,68 @@ class EmployeesController extends Controller
       }
          
       }
+      public function update($id, Request $request){
+        // cari courses berdasarkan id
+        $employees = Employees::find($id); // SELECT * FROM courses WHERE id = $id;
+
+        if ($employees) {
+            //simpan perubahan
+            $employees->update([
+             'nama pegawai' => $request->name ?? $employees->name,
+                 'jenis kelamin pegawai'=> $request->gender ?? $employees->gender,
+                 'no hp pegawai'=> $request->phone ?? $employees->phone,
+                 'alamat pegawai'=> $request->address ?? $employees->address,
+                 'email pegawai' => $request->email ?? $employees->email,
+                 'status pegawai' => $request->status ?? $employees->status,
+                 'tanggal masuk kerja' => $request->hired_on ?? $employees->hired_on,
+                 
+             
+            ]);
+            $data = [
+             'message' => 'Data Berhasil Diupdate',
+             'data' => $employees
+         ];
+         return response()->json($data, 200);
+            
+        }else{
+            $data = [
+                'message' => 'Data tidak diubah',
+                
+            ];
+            return response()->json($data, 404);
+        }
+
+
+
+
+
+
+
+    }
+     public function destroy($id){
+        // cari courses berdasarkan id
+        $employees = Employees::find($id); // SELECT * FROM courses WHERE id = $id;
+
+        if($employees){
+            $data = [
+                'message' => 'Data Berhasil Dihapus',
+                'data' => $employees
+            ];
+    
+            // hapus courses
+            $employees->delete();
+    
+            return response()->json($data, 200);
+
+        }else{
+            $data = [
+                'message' => 'Data tidak ditemukan',
+                
+            ];
+            return response()->json($data, 404);
+        }
+
+        
+
+    }
 }
--- app/Models/Employees.php
+++ app/Models/Employees.php
@@ -8,4 +8,8 @@ class Employees extends Model
 {
     protected $table = 'employees';
 
+    protected $fillable = ['name', 'gender', 'phone', 'address', 'email', 'status', 'hired_on'];
+
+    public $timestamps = false;
+
 }
